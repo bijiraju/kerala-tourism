8246472f0f12be7e86df48431ff3cf870c4d2edc
bijiraju <bijiraju1992@gmail.com>
2022-01-27 17:45:32 +0530

    changed

--- assets/js/main.js
+++ assets/js/main.js
@@ -156,37 +156,40 @@
 
 })()
 
+  /**
+   * Validations 
+   */
 const form = document.querySelector("form");
-eField = form.querySelector(".email"),
-eInput = eField.querySelector("input"),
-pField = form.querySelector(".password"),
-pInput = pField.querySelector("input");
+emailField = form.querySelector(".email"),
+emailInput = emailField.querySelector("input"),
+passwordField = form.querySelector(".password"),
+passwordInput = passwordField.querySelector("input");
 
 form.onsubmit = (e)=>{
   e.preventDefault(); //preventing from form submitting
   //if email and password is blank then add shake class in it else call specified function
-  (eInput.value == "") ? eField.classList.add("shake", "error") : checkEmail();
-  (pInput.value == "") ? pField.classList.add("shake", "error") : checkPass();
+  (emailInput.value == "") ? emailField.classList.add("shake", "error") : checkEmail();
+  (passwordInput.value == "") ? passwordField.classList.add("shake", "error") : checkPass();
 
   setTimeout(()=>{ //remove shake class after 500ms
-    eField.classList.remove("shake");
-    pField.classList.remove("shake");
+    emailField.classList.remove("shake");
+    passwordField.classList.remove("shake");
   }, 500);
 
-  eInput.onkeyup = ()=>{checkEmail();} //calling checkEmail function on email input keyup
-  pInput.onkeyup = ()=>{checkPass();} //calling checkPassword function on pass input keyup
+  emailInput.onkeyup = ()=>{checkEmail();} //calling checkEmail function on email input keyup
+  passwordInput.onkeyup = ()=>{checkPass();} //calling checkPassword function on pass input keyup
 
   function checkEmail(){ //checkEmail function
     let pattern = /^([A-Za-z0-9\.-]+)@([A-Za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/; //pattern for validate email
-    if(!eInput.value.match(pattern)){ //if pattern not matched then add error and remove valid class
-      eField.classList.add("error");
-      eField.classList.remove("valid");
-      let errorTxt = eField.querySelector(".error-txt");
+    if(!emailInput.value.match(pattern)){ //if pattern not matched then add error and remove valid class
+      emailField.classList.add("error");
+      emailField.classList.remove("valid");
+      let errorTxt = emailField.querySelector(".error-txt");
       //if email value is not empty then show please enter valid email else show Email can't be blank
-      (eInput.value != "") ? errorTxt.innerText = "Enter a valid email address" : errorTxt.innerText = "Email can't be blank";
+      (emailInput.value != "") ? errorTxt.innerText = "Enter a valid email address" : errorTxt.innerText = "Email can't be blank";
     }else{ //if pattern matched then remove error and add valid class
-      eField.classList.remove("error");
-      eField.classList.add("valid");
+      emailField.classList.remove("error");
+      emailField.classList.add("valid");
     }
   }
 
@@ -194,37 +197,37 @@ form.onsubmit = (e)=>{
      let strongPassword = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})');
      let mediumPassword = new RegExp('((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{6,}))|((?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=.{8,}))');
 
-    if(pInput.value == ""){ //if pass is empty then add error and remove valid class
-      pField.classList.add("error");
-      pField.classList.remove("valid");
+    if(passwordInput.value == ""){ //if pass is empty then add error and remove valid class
+      passwordField.classList.add("error");
+      passwordField.classList.remove("valid");
     }
-    else if(!pInput.value.match(strongPassword))
+    else if(!passwordInput.value.match(strongPassword))
     {
-        pField.classList.add("error");
-        pField.classList.remove("valid");
+        passwordField.classList.add("error");
+        passwordField.classList.remove("valid");
         let errorTxt = pField.querySelector(".error-txt");
         
       //if email value is not empty then show please enter valid email else show Email can't be blank
-      (pInput.value != "") ? errorTxt.innerText = "Weak" : errorTxt.innerText = "Password can't be blank";
+      (passwordInput.value != "") ? errorTxt.innerText = "Weak" : errorTxt.innerText = "Password can't be blank";
     
     }
-    else if(!pInput.value.match(mediumPassword))
+    else if(!passwordInput.value.match(mediumPassword))
     {
-        pField.classList.add("error");
-        pField.classList.remove("valid");
-        let errorTxt = pField.querySelector(".error-txt");
+        passwordField.classList.add("error");
+        passwordField.classList.remove("valid");
+        let errorTxt = passwordField.querySelector(".error-txt");
       //if email value is not empty then show please enter valid email else show Email can't be blank
-      (pInput.value == mediumPassword) ? errorTxt.innerText = "Medium" : errorTxt.innerText = "Password can't be blank";
+      (passwordInput.value == mediumPassword) ? errorTxt.innerText = "Medium" : errorTxt.innerText = "Password can't be blank";
     
     }
     else{ //if pass is empty then remove error and add valid class
-      pField.classList.remove("error");
-      pField.classList.add("valid");
+      passwordField.classList.remove("error");
+      passwordField.classList.add("valid");
     }
   }
 
   //if eField and pField doesn't contains error class that mean user filled details properly
-  if(!eField.classList.contains("error") && !pField.classList.contains("error")){
+  if(!emailField.classList.contains("error") && !passwordField.classList.contains("error")){
     window.location.href = form.getAttribute("action"); //redirecting user to the specified url which is inside action attribute of form tag
   }
 }
